9ad7ea8c32898dd683a4ab9d17af91bf490a46bf
John Watson <johnwatson484@hotmail.com>
2021-08-22 20:21:22 +0100

    Fix

--- app/enrichment/process-invoice-lines.js
+++ app/enrichment/process-invoice-lines.js
@@ -2,12 +2,11 @@ const enrichInvoiceLine = require('./enrich-invoice-line')
 const validateInvoiceLine = require('./validate-invoice-line')
 
 const processInvoiceLines = async (invoiceLines, paymentRequestId, fundCode) => {
+  // ignore any net lines
+  invoiceLines = invoiceLines.filter(x => !x.description.startsWith('N00'))
   for (const invoiceLine of invoiceLines) {
-    // ignore any net lines
-    if (!invoiceLine.description.startsWith('N00')) {
-      await enrichInvoiceLine(invoiceLine, fundCode)
-      validateInvoiceLine(invoiceLine)
-    }
+    await enrichInvoiceLine(invoiceLine, fundCode)
+    validateInvoiceLine(invoiceLine)
   }
 }
 
