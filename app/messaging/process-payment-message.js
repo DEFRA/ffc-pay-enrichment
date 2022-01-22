1bd045f560ee8e0eaf171a37167c03cde4e75029
John Watson <johnwatson484@hotmail.com>
2022-01-22 18:58:15 +0000

    Add logging

--- app/messaging/process-payment-message.js
+++ app/messaging/process-payment-message.js
@@ -1,12 +1,15 @@
 const enrichPaymentRequest = require('../enrichment')
 const sendProcessingMessage = require('./send-processing-message')
+const util = require('util')
 
 async function processPaymentMessage (message, receiver) {
   try {
     const paymentRequest = message.body
+    console.log('Payment request received:', util.inspect(paymentRequest, false, null, true))
     await enrichPaymentRequest(paymentRequest)
     await sendProcessingMessage(paymentRequest)
     await receiver.completeMessage(message)
+    console.log('Payment request enriched:', util.inspect(paymentRequest, false, null, true))
   } catch (err) {
     console.error('Unable to process payment request:', err)
     await receiver.deadLetterMessage(message)
