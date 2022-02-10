65da4ad1f8c91a07b29e800ba6d9a75500299ad3
John Watson <johnwatson484@hotmail.com>
2022-02-10 21:50:43 +0000

    Update validation

--- app/enrichment/schemas/invoice-line.js
+++ app/enrichment/schemas/invoice-line.js
@@ -4,7 +4,7 @@ module.exports = Joi.object({
   standardCode: Joi.string().allow(''),
   schemeCode: Joi.string().required(),
   accountCode: Joi.string().allow(''),
-  fundCode: Joi.string().required(),
-  description: Joi.string().required(),
+  fundCode: Joi.string().regex(/^[A-Z]{3}\d{2}$/).required(),
+  description: Joi.string().regex(/^[A-Z]{1}\d{2}\s-\s.+$/).required(),
   value: Joi.number().required()
 })
