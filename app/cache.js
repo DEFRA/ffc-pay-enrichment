62a3fc710c614d6521d73c578cce519fc5fadac4
John Watson <johnwatson484@hotmail.com>
2022-02-18 23:01:39 +0000

    Use reference for scheme

--- app/cache.js
+++ app/cache.js
@@ -1,6 +1,6 @@
 const { cache: config } = require('./config')
 const NodeCache = require('node-cache')
-const cache = new NodeCache()
+const cache = new NodeCache({ useClones: false })
 
 const get = (key) => {
   return cache.get(key)
