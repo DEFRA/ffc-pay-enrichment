57e8742b340f33f0fdc6316b9ace8ed007d2536b
John Watson <johnwatson484@hotmail.com>
2022-06-01 16:24:11 +0100

    Add try/catch to invoice

--- app/enrichment/create-invoice-number.js
+++ app/enrichment/create-invoice-number.js
@@ -1,7 +1,11 @@
 const createInvoiceNumber = (paymentRequest) => {
-  return `S${paymentRequest.invoiceNumber
+  try {
+    return `S${paymentRequest.invoiceNumber
     ? paymentRequest.invoiceNumber.slice(-7)
     : paymentRequest.agreementNumber.slice(-7)}${paymentRequest.contractNumber}V${paymentRequest.paymentRequestNumber.toString().padStart(3, '0')}`
+  } catch {
+    return undefined
+  }
 }
 
 module.exports = createInvoiceNumber
