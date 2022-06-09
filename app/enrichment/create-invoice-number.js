e7a6fcb452cda0802e5264865b926d1f403e59d4
John Watson <johnwatson484@hotmail.com>
2022-06-09 17:19:41 +0100

    Update invoice handling

--- app/enrichment/create-invoice-number.js
+++ app/enrichment/create-invoice-number.js
@@ -1,10 +1,27 @@
 const createInvoiceNumber = (paymentRequest) => {
   try {
-    return `S${paymentRequest.invoiceNumber
-    ? paymentRequest.invoiceNumber.slice(-7)
-    : paymentRequest.agreementNumber.slice(-7)}${paymentRequest.contractNumber}V${paymentRequest.paymentRequestNumber.toString().padStart(3, '0')}`
+    switch (paymentRequest.schemeId) {
+      case 1:
+      case 2:
+      case 3:
+        return createSitiAgriInvoiceNumber(paymentRequest)
+      default:
+        return createDefaultInvoiceNumber(paymentRequest)
+    }
   } catch {
     return undefined
+  }
+}
+
+const createSitiAgriInvoiceNumber = (paymentRequest) => {
+  if (paymentRequest.invoiceNumber.length >= 7 && paymentRequest.contractNumber && paymentRequest.paymentRequestNumber) {
+    return `S${paymentRequest.invoiceNumber.slice(-7)}${paymentRequest.contractNumber}V${paymentRequest.paymentRequestNumber.toString().padStart(3, '0')}`
+  }
+}
+
+const createDefaultInvoiceNumber = (paymentRequest) => {
+  if (paymentRequest.agreementNumber && paymentRequest.paymentRequestNumber) {
+    return `${paymentRequest.agreementNumber}V${paymentRequest.paymentRequestNumber.toString().padStart(3, '0')}`
   }
 }
 
