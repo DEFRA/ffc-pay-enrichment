c7cbe6cf53656da348fc80e9fc6057596462efd2
John Watson <johnwatson484@hotmail.com>
2022-06-09 16:24:13 +0100

    Fix spaces

--- app/enrichment/validate-type.js
+++ app/enrichment/validate-type.js
@@ -1,6 +1,6 @@
 const { VALIDATION } = require('../errors')
 
-const validateType = (paymentRequest) => {  
+const validateType = (paymentRequest) => {
   if (typeof paymentRequest !== 'object' || Array.isArray(paymentRequest) || !paymentRequest) {
     const error = new Error('Payment request structure is invalid. Must be an object.')
     error.category = VALIDATION
