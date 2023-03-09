db6950d0fcb043da1647e0e1d0fe6c6c4e4c6010
John Watson <john.watson2@rpa.gov.uk>
2023-03-09 15:22:12 +0000

    Add tests

--- test/unit/event/send-enrichment-error-event.test.js
+++ test/unit/event/send-enrichment-error-event.test.js
@@ -1,43 +1,72 @@
-jest.mock('../../../app/event/raise-event')
-const raiseEvent = require('../../../app/event/raise-event')
+const mockSendEvent = jest.fn()
+const mockPublishEvent = jest.fn()
+jest.mock('ffc-pay-event-publisher', () => {
+  return {
+    PublishEvent: jest.fn().mockImplementation(() => {
+      return {
+        sendEvent: mockSendEvent
+      }
+    }),
+    EventPublisher: jest.fn().mockImplementation(() => {
+      return {
+        publishEvent: mockPublishEvent
+      }
+    })
+  }
+})
+jest.mock('../../../app/config')
+const config = require('../../../app/config')
 const sendEnrichErrorEvent = require('../../../app/event/send-enrichment-error-event')
 const mockPaymentRequest = require('../../mock-payment-request')
 
 let paymentRequest
 let error
 
-describe('Payment requests that cannot be enriched', () => {
-  beforeEach(async () => {
+describe('V1 enrichment error event', () => {
+  beforeEach(() => {
     paymentRequest = mockPaymentRequest
-
     error = {
       message: 'Cannot enrich'
     }
+    config.useV1Events = true
+    config.useV2Events = false
   })
 
-  afterEach(async () => {
-    jest.resetAllMocks()
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 
-  test('should call raiseEvent when a valid paymentRequest is received', async () => {
+  test('should sent event if V1 events enabled', async () => {
+    config.useV1Events = true
     await sendEnrichErrorEvent(paymentRequest, error)
-    expect(raiseEvent).toHaveBeenCalled()
+    expect(mockSendEvent).toHaveBeenCalled()
   })
 
-  test('should use existing correlationId if provided', async () => {
+  test('should not sent event if V1 events disabled', async () => {
+    config.useV1Events = false
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockSendEvent).not.toHaveBeenCalled()
+  })
+
+  test('should raise an event when an invalid paymentRequest is received', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockSendEvent).toHaveBeenCalled()
+  })
+
+  test('should raise an event with payment request correlation Id as Id if exists', async () => {
     paymentRequest.correlationId = '591f9694-7851-47f7-98b3-b1c710c363d8'
     await sendEnrichErrorEvent(paymentRequest, error)
-    expect(raiseEvent.mock.calls[0][0].id).toBe(paymentRequest.correlationId)
+    expect(mockSendEvent.mock.calls[0][0].properties.id).toBe(paymentRequest.correlationId)
   })
 
-  test('should use generate correlationId if not provided', async () => {
+  test('should generate a new uuid as Id if payment request does not have correlation Id', async () => {
     await sendEnrichErrorEvent(paymentRequest, error)
-    expect(raiseEvent.mock.calls[0][0].id).toMatch(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
   })
 
   test('should use map error message if provided', async () => {
     await sendEnrichErrorEvent(paymentRequest, error)
-    expect(raiseEvent.mock.calls[0][0].message).toBe(error.message)
+    expect(mockSendEvent.mock.calls[0][0].properties.action.message).toBe(error.message)
   })
 
   test('should throw error if no error provided', async () => {
