02591d4a0c8632a8893b2343de556d994b88c0ad
John Watson <johnwatson484@hotmail.com>
2023-03-09 20:26:38 +0000

    Add tests

--- test/unit/event/send-enrichment-error-event.test.js
+++ test/unit/event/send-enrichment-error-event.test.js
@@ -22,27 +22,27 @@ const mockPaymentRequest = require('../../mock-payment-request')
 let paymentRequest
 let error
 
+beforeEach(() => {
+  paymentRequest = mockPaymentRequest
+  error = {
+    message: 'Cannot enrich'
+  }
+  config.useV1Events = true
+  config.useV2Events = true
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
 describe('V1 enrichment error event', () => {
-  beforeEach(() => {
-    paymentRequest = mockPaymentRequest
-    error = {
-      message: 'Cannot enrich'
-    }
-    config.useV1Events = true
-    config.useV2Events = false
-  })
-
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
-
-  test('should sent event if V1 events enabled', async () => {
+  test('should send V1 event if V1 events enabled', async () => {
     config.useV1Events = true
     await sendEnrichErrorEvent(paymentRequest, error)
     expect(mockSendEvent).toHaveBeenCalled()
   })
 
-  test('should not sent event if V1 events disabled', async () => {
+  test('should not send V1 event if V1 events disabled', async () => {
     config.useV1Events = false
     await sendEnrichErrorEvent(paymentRequest, error)
     expect(mockSendEvent).not.toHaveBeenCalled()
@@ -61,15 +61,74 @@ describe('V1 enrichment error event', () => {
 
   test('should generate a new uuid as Id if payment request does not have correlation Id', async () => {
     await sendEnrichErrorEvent(paymentRequest, error)
-    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
   })
 
-  test('should use map error message if provided', async () => {
+  test('should raise payment-request-enrichment-error event name', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-enrichment-error')
+  })
+
+  test('should raise error status event', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockSendEvent.mock.calls[0][0].properties.status).toBe('error')
+  })
+
+  test('should raise error event type', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockSendEvent.mock.calls[0][0].properties.action.type).toBe('error')
+  })
+
+  test('should include error message in event', async () => {
     await sendEnrichErrorEvent(paymentRequest, error)
     expect(mockSendEvent.mock.calls[0][0].properties.action.message).toBe(error.message)
   })
 
+  test('should include payment request in event', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockSendEvent.mock.calls[0][0].properties.action.data.paymentRequest).toEqual(paymentRequest)
+  })
+
   test('should throw error if no error provided', async () => {
     await expect(() => sendEnrichErrorEvent(paymentRequest)).rejects.toThrow()
+  })
+})
+
+describe('V2 enrichment error event', () => {
+  test('should send V2 event if V2 events enabled', async () => {
+    config.useV2Events = true
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockPublishEvent).toHaveBeenCalled()
+  })
+
+  test('should not send V2 event if V2 events disabled', async () => {
+    config.useV2Events = false
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockPublishEvent).not.toHaveBeenCalled()
+  })
+
+  test('should raise an event when an invalid paymentRequest is received', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockPublishEvent).toHaveBeenCalled()
+  })
+
+  test('should raise an event with enrichment source', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockPublishEvent.mock.calls[0][0].source).toBe('ffc-pay-enrichment')
+  })
+
+  test('should raise rejected payment event type', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockPublishEvent.mock.calls[0][0].type).toBe('uk.gov.defra.ffc.pay.warning.payment.rejected')
+  })
+
+  test('should include error message in event data', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockPublishEvent.mock.calls[0][0].data.message).toBe(error.message)
+  })
+
+  test('should include payment request in event data', async () => {
+    await sendEnrichErrorEvent(paymentRequest, error)
+    expect(mockPublishEvent.mock.calls[0][0].data.paymentRequest).toEqual(paymentRequest)
   })
 })
