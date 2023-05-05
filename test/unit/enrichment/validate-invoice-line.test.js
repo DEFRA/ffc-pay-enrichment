890ff0869f8300e3e029e1da64458ba543b44764
Kirsten Williamson <kirsten.williamson@kainos.com>
2023-05-05 12:25:28 +0100

    Restructure some test files

--- test/unit/enrichment/validate-invoice-line.test.js
+++ test/unit/enrichment/validate-invoice-line.test.js
@@ -1,12 +1,12 @@
-const { VALIDATION } = require('../../../app/constants/errors')
-
 jest.mock('../../../app/enrichment/schemas/invoice-line')
 const mockSchema = require('../../../app/enrichment/schemas/invoice-line')
 
-const validateInvoiceLine = require('../../../app/enrichment/validate-invoice-line')
+const { VALIDATION } = require('../../../app/constants/errors')
 
 const invoiceLine = require('../../mocks/payment-requests/invoice-line')
 const sourceSystem = require('../../mocks/values/source-system')
+
+const validateInvoiceLine = require('../../../app/enrichment/validate-invoice-line')
 
 describe('validate header', () => {
   beforeEach(() => {
