071936fb67db65c669746c14b0f7b0887874100c
John Watson <johnwatson484@hotmail.com>
2023-06-23 19:44:10 +0100

    Add bs

--- app/enrichment/get-balancing-segment.js
+++ app/enrichment/get-balancing-segment.js
@@ -1,5 +1,10 @@
 const getBalancingSegment = (invoiceLine) => {
-
+  if (invoiceLine.companyCode === '31') {
+    return 'EXQ'
+  }
+  if (invoiceLine.companyCode === '32') {
+    return 'EGF'
+  }
 }
 
 module.exports = {
