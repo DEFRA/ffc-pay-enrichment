d24e52367687774e3ff50974af896bea718693cb
John Watson <john.watson1@defra.gov.uk>
2023-06-27 16:49:15 +0100

    Add tests

--- test/unit/enrichment/enrich-invoice-line.test.js
+++ test/unit/enrichment/enrich-invoice-line.test.js
@@ -1,11 +1,25 @@
+jest.mock('../../../app/currency-convert')
+const { convertToPence: mockConvertToPence } = require('../../../app/currency-convert')
+
 jest.mock('../../../app/enrichment/get-scheme-code')
 const { getSchemeCode: mockGetSchemeCode } = require('../../../app/enrichment/get-scheme-code')
+
+jest.mock('../../../app/enrichment/get-fund-code')
+const { getFundCode: mockGetFundCode } = require('../../../app/enrichment/get-fund-code')
+
+jest.mock('../../../app/enrichment/get-delivery-body')
+const { getDeliveryBody: mockGetDeliveryBody } = require('../../../app/enrichment/get-delivery-body')
+
+jest.mock('../../../app/enrichment/get-marketing-year')
+const { getMarketingYear: mockGetMarketingYear } = require('../../../app/enrichment/get-marketing-year')
 
 jest.mock('../../../app/enrichment/is-state-aid')
 const { isStateAid: mockIsStateAid } = require('../../../app/enrichment/is-state-aid')
 
 const scheme = require('../../mocks/scheme')
 const { SCHEME_CODE } = require('../../mocks/values/scheme-code')
+const { FUND_CODE } = require('../../mocks/values/fund-code')
+const { DELIVERY_BODY_RPA } = require('../../mocks/values/delivery-body')
 
 const { enrichInvoiceLine } = require('../../../app/enrichment/enrich-invoice-line')
 
@@ -17,96 +31,55 @@ describe('enrich header', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
+    mockConvertToPence.mockReturnValue(25000)
     mockGetSchemeCode.mockReturnValue(SCHEME_CODE)
+    mockGetFundCode.mockReturnValue(FUND_CODE)
+    mockGetDeliveryBody.mockReturnValue(DELIVERY_BODY_RPA)
+    mockGetMarketingYear.mockReturnValue(marketingYear)
     mockIsStateAid.mockReturnValue(false)
 
     invoiceLine = JSON.parse(JSON.stringify(require('../../mocks/payment-requests/invoice-line')))
   })
 
-  test('should convert value to pence', () => {
+  test('should convert value to pence', async () => {
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.value).toBe(25000)
+    expect(mockConvertToPence).toHaveBeenCalledWith(250)
   })
 
-  test('should retain scheme code if already set', () => {
-    const originalSchemeCode = invoiceLine.schemeCode
+  test('should get scheme code', async () => {
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.schemeCode).toBe(originalSchemeCode)
+    expect(mockGetSchemeCode).toHaveBeenCalledWith(invoiceLine)
   })
 
-  test('should set scheme code if not already set', () => {
-    delete invoiceLine.schemeCode
+  test('should get fund code', async () => {
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.schemeCode).toBe(SCHEME_CODE)
+    expect(mockGetFundCode).toHaveBeenCalledWith(invoiceLine, FUND_CODE)
   })
 
-  test('should retain fund code if already set', () => {
-    const originalFundCode = invoiceLine.fundCode
-    enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.fundCode).toBe(originalFundCode)
-  })
-
-  test('should set fund code if not already set and fund code defined', () => {
-    delete invoiceLine.fundCode
-    enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.fundCode).toBe(scheme.fundCode)
-  })
-
-  test('should not set fund code if not already set and fund code not defined', () => {
-    delete invoiceLine.fundCode
-    enrichInvoiceLine(invoiceLine, undefined)
-    expect(invoiceLine.fundCode).toBeUndefined()
-  })
-
-  test('should retain convergence if already set', () => {
-    invoiceLine.convergence = true
-    enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.convergence).toBe(true)
-  })
-
-  test('should set convergence to false if not already set', () => {
+  test('should set convergence to false if not set', async () => {
     delete invoiceLine.convergence
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
     expect(invoiceLine.convergence).toBe(false)
   })
 
-  test('should retain delivery body if already set', () => {
-    const originalDeliveryBody = invoiceLine.deliveryBody
+  test('should set convergence to true if set', async () => {
+    invoiceLine.convergence = true
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.deliveryBody).toBe(originalDeliveryBody)
+    expect(invoiceLine.convergence).toBe(true)
   })
 
-  test('should set delivery body if not already set and delivery body defined', () => {
-    delete invoiceLine.deliveryBody
+  test('should get delivery body', async () => {
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.deliveryBody).toBe(scheme.deliveryBody)
+    expect(mockGetDeliveryBody).toHaveBeenCalledWith(invoiceLine, DELIVERY_BODY_RPA)
   })
 
-  test('should retain marketing year if already set', () => {
-    const originalMarketingYear = invoiceLine.marketingYear
+  test('should get marketing year', async () => {
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.marketingYear).toBe(originalMarketingYear)
+    expect(mockGetMarketingYear).toHaveBeenCalledWith(invoiceLine, marketingYear)
   })
 
-  test('should set marketing year if not already set', () => {
-    delete invoiceLine.marketingYear
-    enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.marketingYear).toBe(marketingYear)
-  })
-
-  test('should check if line is state aid', () => {
+  test('should get state aid', async () => {
     enrichInvoiceLine(invoiceLine, marketingYear, scheme)
     expect(mockIsStateAid).toHaveBeenCalledWith(invoiceLine)
-  })
-
-  test('should set state aid when state aid is false', () => {
-    enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.stateAid).toBe(false)
-  })
-
-  test('should set state aid when state aid is true', () => {
-    mockIsStateAid.mockReturnValue(true)
-    enrichInvoiceLine(invoiceLine, marketingYear, scheme)
-    expect(invoiceLine.stateAid).toBe(true)
   })
 })
