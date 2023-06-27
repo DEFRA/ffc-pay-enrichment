0c731e160bf74445e13f440316806abb4c750e0c
John Watson <john.watson1@defra.gov.uk>
2023-06-27 17:11:43 +0100

    Add tests

--- test/mocks/payment-requests/es.js
+++ test/mocks/payment-requests/es.js
@@ -2,10 +2,12 @@ const paymentRequest = require('./payment-request')
 const esInvoiceLine = require('./es-invoice-line')
 const { ES } = require('../../../app/constants/schemes')
 const { ES_INVOICE_NUMBER } = require('../values/invoice-number')
+const { VENDOR } = require('../values/vendor')
 
 module.exports = {
   ...paymentRequest,
   schemeId: ES,
   invoiceNumber: ES_INVOICE_NUMBER,
+  vendor: VENDOR,
   invoiceLines: [esInvoiceLine]
 }
