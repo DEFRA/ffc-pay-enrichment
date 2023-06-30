b22021eeb2094c7896ea763d4cfdde447f40feaa
John Watson <john.watson1@defra.gov.uk>
2023-06-30 17:01:01 +0100

    Add date format type

--- app/date-convert.js
+++ app/date-convert.js
@@ -14,7 +14,7 @@ const getCurrentDate = () => {
 
 const formatDate = (dateString) => {
   try {
-    const date = moment(dateString, [DAX_DATE_FORMAT, SITI_AGRI_DATE_FORMAT, 'DD-MM-YYYY', 'YYYY/MM/DD'], true)
+    const date = moment(dateString, [DAX_DATE_FORMAT, SITI_AGRI_DATE_FORMAT, 'DD-MM-YYYY', 'YYYY/MM/DD', 'DD-MMM-YY'], true)
     return date.isValid() ? date.format(DAX_DATE_FORMAT) : undefined
   } catch {
     return undefined
