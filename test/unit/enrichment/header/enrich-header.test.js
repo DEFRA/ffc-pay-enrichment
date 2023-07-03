8d675c4ac917ae773d2a40609b3b74326d6aa70c
John Watson <john.watson1@defra.gov.uk>
2023-07-03 16:33:41 +0100

    Add tests

--- test/unit/enrichment/header/enrich-header.test.js
+++ test/unit/enrichment/header/enrich-header.test.js
@@ -1,8 +1,8 @@
-const { AP } = require('../../../../app/constants/ledgers')
-const { SFI_INVOICE_NUMBER } = require('../../../mocks/values/invoice-number')
-const { FRN } = require('../../../mocks/values/frn')
-const { DUE_DATE_DAX } = require('../../../mocks/values/due-date')
-const { GBP } = require('../../../../app/constants/currency')
+jest.mock('../../../../app/enrichment/header/get-correlation-id')
+const { getCorrelationId: mockGetCorrelationId } = require('../../../../app/enrichment/header/get-correlation-id')
+
+jest.mock('../../../../app/enrichment/header/get-agreement-number')
+const { getAgreementNumber: mockGetAgreementNumber } = require('../../../../app/enrichment/header/get-agreement-number')
 
 jest.mock('../../../../app/enrichment/header/create-invoice-number')
 const { createInvoiceNumber: mockCreateInvoiceNumber } = require('../../../../app/enrichment/header/create-invoice-number')
@@ -10,8 +10,30 @@ const { createInvoiceNumber: mockCreateInvoiceNumber } = require('../../../../ap
 jest.mock('../../../../app/enrichment/header/get-frn')
 const { getFrn: mockGetFrn } = require('../../../../app/enrichment/header/get-frn')
 
+jest.mock('../../../../app/enrichment/header/get-ledger')
+const { getLedger: mockGetLedger } = require('../../../../app/enrichment/header/get-ledger')
+
+jest.mock('../../../../app/enrichment/header/get-currency')
+const { getCurrency: mockGetCurrency } = require('../../../../app/enrichment/header/get-currency')
+
+jest.mock('../../../../app/enrichment/header/get-value')
+const { getValue: mockGetValue } = require('../../../../app/enrichment/header/get-value')
+
 jest.mock('../../../../app/enrichment/header/confirm-due-date')
 const { confirmDueDate: mockConfirmDueDate } = require('../../../../app/enrichment/header/confirm-due-date')
+
+jest.mock('../../../../app/date-convert')
+const { convertToDaxDate: mockConvertToDaxDate } = require('../../../../app/date-convert')
+
+const { CORRELATION_ID } = require('../../../mocks/values/correlation-id')
+const { AGREEMENT_NUMBER } = require('../../../mocks/values/agreement-number')
+const { SFI_INVOICE_NUMBER } = require('../../../mocks/values/invoice-number')
+const { FRN } = require('../../../mocks/values/frn')
+const { DUE_DATE_DAX } = require('../../../mocks/values/due-date')
+const { EVENT_DATE_DAX } = require('../../../mocks/values/event-date')
+
+const { AP } = require('../../../../app/constants/ledgers')
+const { GBP } = require('../../../../app/constants/currency')
 
 const { enrichHeader } = require('../../../../app/enrichment/header/enrich-header')
 
@@ -22,24 +44,29 @@ describe('enrich header', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
+    mockGetCorrelationId.mockReturnValue(CORRELATION_ID)
+    mockGetAgreementNumber.mockReturnValue(AGREEMENT_NUMBER)
     mockCreateInvoiceNumber.mockReturnValue(SFI_INVOICE_NUMBER)
     mockGetFrn.mockResolvedValue(FRN)
+    mockGetLedger.mockReturnValue(AP)
+    mockGetCurrency.mockReturnValue(GBP)
+    mockGetValue.mockReturnValue(100)
     mockConfirmDueDate.mockReturnValue(DUE_DATE_DAX)
+    mockConvertToDaxDate.mockReturnValue(EVENT_DATE_DAX)
 
     scheme = JSON.parse(JSON.stringify(require('../../../mocks/scheme')))
     paymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/payment-request')))
   })
 
-  test('should retain correlation id if already exists', async () => {
-    const originalCorrelationId = paymentRequest.correlationId
+  test('should set delivery body if scheme defined', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.correlationId).toBe(originalCorrelationId)
+    expect(paymentRequest.deliveryBody).toBe(scheme.deliveryBody)
   })
 
-  test('should create uuid correlation id if does not exist', async () => {
-    delete paymentRequest.correlationId
-    await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.correlationId).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/)
+  test('should not set delivery body if scheme not defined', async () => {
+    scheme = undefined
+    await enrichHeader(paymentRequest)
+    expect(paymentRequest.deliveryBody).toBeUndefined()
   })
 
   test('should set scheme id if scheme defined', async () => {
@@ -53,79 +80,93 @@ describe('enrich header', () => {
     expect(paymentRequest.schemeId).toBeUndefined()
   })
 
-  test('should retain agreement number if already set', async () => {
-    const originalAgreementNumber = paymentRequest.agreementNumber
+  test('should get correlation id from existing correlation id', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.agreementNumber).toBe(originalAgreementNumber)
+    expect(mockGetCorrelationId).toHaveBeenCalledWith(paymentRequest.correlationId)
   })
 
-  test('should set agreement number to contract number if not already set', async () => {
-    delete paymentRequest.agreementNumber
+  test('should set correlation id', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.agreementNumber).toBe(paymentRequest.contractNumber)
+    expect(paymentRequest.correlationId).toBe(CORRELATION_ID)
+  })
+
+  test('should get agreement number for payment request', async () => {
+    await enrichHeader(paymentRequest, scheme)
+    expect(mockGetAgreementNumber).toHaveBeenCalledWith(paymentRequest)
+  })
+
+  test('should set agreement number', async () => {
+    await enrichHeader(paymentRequest, scheme)
+    expect(paymentRequest.agreementNumber).toBe(AGREEMENT_NUMBER)
+  })
+
+  test('should get invoice number', async () => {
+    await enrichHeader(paymentRequest, scheme)
+    expect(mockCreateInvoiceNumber).toHaveBeenCalledWith(paymentRequest)
   })
 
   test('should set invoice number', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(mockCreateInvoiceNumber).toHaveBeenCalledWith(paymentRequest)
     expect(paymentRequest.invoiceNumber).toBe(SFI_INVOICE_NUMBER)
   })
 
-  test('should retain ledger if already set', async () => {
-    const originalLedger = paymentRequest.ledger
+  test('should get frn', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.ledger).toBe(originalLedger)
+    expect(mockGetFrn).toHaveBeenCalledWith(paymentRequest)
   })
 
-  test('should set ledger to AP if not already set', async () => {
-    delete paymentRequest.ledger
+  test('should set frn', async () => {
+    await enrichHeader(paymentRequest, scheme)
+    expect(paymentRequest.frn).toBe(FRN)
+  })
+
+  test('should get ledger', async () => {
+    await enrichHeader(paymentRequest, scheme)
+    expect(mockGetLedger).toHaveBeenCalledWith(paymentRequest.ledger)
+  })
+
+  test('should set ledger', async () => {
     await enrichHeader(paymentRequest, scheme)
     expect(paymentRequest.ledger).toBe(AP)
   })
 
-  test('should convert value to pence', async () => {
+  test('should get currency', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.value).toBe(15000)
+    expect(mockGetCurrency).toHaveBeenCalledWith(paymentRequest.currency)
   })
 
-  test('should retain frn if already set', async () => {
-    const originalFrn = paymentRequest.frn
+  test('should set currency', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.frn).toBe(originalFrn)
+    expect(paymentRequest.currency).toBe(GBP)
   })
 
-  test('should set frn if not already set', async () => {
-    delete paymentRequest.frn
+  test('should get value', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(mockGetFrn).toHaveBeenCalledWith(paymentRequest)
-    expect(paymentRequest.frn).toBe(FRN)
+    expect(mockGetValue).toHaveBeenCalledWith(paymentRequest)
   })
 
-  test('should set delivery body', async () => {
+  test('should set value', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.deliveryBody).toBe(scheme.deliveryBody)
+    expect(paymentRequest.value).toBe(100)
   })
 
-  test('should not set delivery body if scheme not defined', async () => {
-    scheme = undefined
-    await enrichHeader(paymentRequest)
-    expect(paymentRequest.deliveryBody).toBeUndefined()
+  test('should get due date', async () => {
+    await enrichHeader(paymentRequest, scheme)
+    expect(mockConfirmDueDate).toHaveBeenCalledWith(paymentRequest.schemeId, paymentRequest.marketingYear, paymentRequest.dueDate)
   })
 
-  test('should confirm due with DAX format', async () => {
+  test('should set due date', async () => {
     await enrichHeader(paymentRequest, scheme)
     expect(paymentRequest.dueDate).toBe(DUE_DATE_DAX)
   })
 
-  test('should retain currency if already set', async () => {
-    const originalCurrency = paymentRequest.currency
+  test('should get event date', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.currency).toBe(originalCurrency)
+    expect(mockConvertToDaxDate).toHaveBeenCalledWith(paymentRequest.eventDate, false)
   })
 
-  test('should set currency to GBP if not already set', async () => {
-    delete paymentRequest.currency
+  test('should set event date', async () => {
     await enrichHeader(paymentRequest, scheme)
-    expect(paymentRequest.currency).toBe(GBP)
+    expect(paymentRequest.eventDate).toBe(EVENT_DATE_DAX)
   })
 })
