e3306b83fdb782ca50c45902487fb539fe5b03e1
John Watson <john.watson1@defra.gov.uk>
2023-07-05 17:51:39 +0100

    Add agreement number

--- app/enrichment/header/get-agreement-number.js
+++ app/enrichment/header/get-agreement-number.js
@@ -1,4 +1,9 @@
+const { FC } = require('../../constants/schemes')
+
 const getAgreementNumber = (paymentRequest) => {
+  if (paymentRequest.schemeId === FC) {
+    return paymentRequest.invoiceNumber?.split(' ')[0]
+  }
   return paymentRequest.agreementNumber ?? paymentRequest.contractNumber
 }
 
