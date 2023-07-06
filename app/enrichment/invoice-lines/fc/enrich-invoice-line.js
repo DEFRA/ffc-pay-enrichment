ab3ac0b59282f84ff3a7e160c0f322907fbcea90
John Watson <john.watson1@defra.gov.uk>
2023-07-06 11:45:44 +0100

    Add tests

--- app/enrichment/invoice-lines/fc/enrich-invoice-line.js
+++ app/enrichment/invoice-lines/fc/enrich-invoice-line.js
@@ -1,7 +1,7 @@
 const { getFundCode } = require('./get-fund-code')
 const { getMarketingYear } = require('./get-marketing-year')
 const { getAccountCode } = require('./get-account-code')
-const { getDeliveryBody } = require('../get-delivery-body')
+const { getDeliveryBody } = require('./get-delivery-body')
 
 const enrichInvoiceLine = (invoiceLine) => {
   invoiceLine.fundCode = getFundCode(invoiceLine.standardCode)
