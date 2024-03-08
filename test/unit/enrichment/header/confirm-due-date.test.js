4d79c98116425d3dcbb70c1ee0383a95f28530ec
Sam Plackett <samuel.plackett@eviden.com>
2024-03-08 11:52:28 +0000

    add tests

--- test/unit/enrichment/header/confirm-due-date.test.js
+++ test/unit/enrichment/header/confirm-due-date.test.js
@@ -1,12 +1,16 @@
+const moment = require('moment')
+
 jest.mock('../../../../app/date-convert')
 const { convertToDaxDate: mockConvertToDaxDate } = require('../../../../app/date-convert')
 
 const { DUE_DATE_DAX } = require('../../../mocks/values/due-date')
 
 const { confirmDueDate } = require('../../../../app/enrichment/header/confirm-due-date')
+const { SITI_AGRI_DATE_FORMAT } = require('../../../../app/constants/date-formats')
 
 let paymentRequest
 let bpsPaymentRequest
+let csPaymentRequest
 
 describe('confirm due date', () => {
   beforeEach(() => {
@@ -14,16 +18,37 @@ describe('confirm due date', () => {
     mockConvertToDaxDate.mockReturnValue(DUE_DATE_DAX)
     paymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/payment-request')))
     bpsPaymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/bps')))
+    csPaymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/cs')))
   })
 
-  test('should convert due date as is if not BPS', () => {
+  test('should convert due date as is if not BPS OR CS', () => {
     confirmDueDate(paymentRequest.schemeId, paymentRequest.marketingYear, paymentRequest.dueDate)
     expect(mockConvertToDaxDate).toHaveBeenCalledWith(paymentRequest.dueDate)
   })
 
-  test('should convert due date to 1st December of marketing year if BPS', () => {
+  test('should convert due date to 1st December of marketing year if BPS, provided this is in the future', () => {
+    bpsPaymentRequest.marketingYear = moment().startOf('day').add(1, 'year').year()
     confirmDueDate(bpsPaymentRequest.schemeId, bpsPaymentRequest.marketingYear, bpsPaymentRequest.dueDate)
-    expect(mockConvertToDaxDate).toHaveBeenCalledWith('2022-12-01')
+    expect(mockConvertToDaxDate).toHaveBeenCalledWith(`${bpsPaymentRequest.marketingYear}-12-01`)
+  })
+
+  test('should convert due date to current date if 1st December date is not in future for BPS', () => {
+    bpsPaymentRequest.marketingYear = '2016'
+    confirmDueDate(bpsPaymentRequest.schemeId, bpsPaymentRequest.marketingYear, bpsPaymentRequest.dueDate)
+    expect(mockConvertToDaxDate).toHaveBeenCalledWith(moment().startOf('day').format(SITI_AGRI_DATE_FORMAT))
+  })
+
+  test('should convert due date to current date if 1st December date is not in future for CS', () => {
+    csPaymentRequest.dueDate = '2015-01-01'
+    confirmDueDate(csPaymentRequest.schemeId, csPaymentRequest.marketingYear, csPaymentRequest.dueDate)
+    expect(mockConvertToDaxDate).toHaveBeenCalledWith(moment().startOf('day').format(SITI_AGRI_DATE_FORMAT))
+  })
+
+  test('should throw error for BPS/CS if date is in unexpected bad format', () => {
+    csPaymentRequest.dueDate = '01/01/2023'
+    const confirmDueDateWrapper = () => confirmDueDate(csPaymentRequest.schemeId, csPaymentRequest.marketingYear, csPaymentRequest.dueDate)
+    expect(confirmDueDateWrapper).toThrow()
+    expect(mockConvertToDaxDate).not.toHaveBeenCalled()
   })
 
   test('should return converted due date', () => {
