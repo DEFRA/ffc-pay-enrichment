a3d03c77ed02d45f119affed9e10b6f08f6b87f2
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-03-12 09:26:27 +0000

    apply amendment to due date (#89)

--- app/enrichment/header/confirm-due-date.js
+++ app/enrichment/header/confirm-due-date.js
@@ -1,9 +1,22 @@
-const { BPS } = require('../../constants/schemes')
+const moment = require('moment')
+const { BPS, CS } = require('../../constants/schemes')
+const { SITI_AGRI_DATE_FORMAT } = require('../../constants/date-formats')
 const { convertToDaxDate } = require('../../date-convert')
 
 const confirmDueDate = (schemeId, marketingYear, dueDate) => {
   if (schemeId === BPS) {
     dueDate = `${marketingYear}-12-01`
+  }
+  if ([BPS, CS].includes(schemeId)) {
+    const currentDate = moment().startOf('day')
+    const dateAsMoment = moment(dueDate, [SITI_AGRI_DATE_FORMAT], true)
+    if (dateAsMoment.isValid()) {
+      if (dateAsMoment.isBefore(currentDate)) {
+        dueDate = currentDate.format(SITI_AGRI_DATE_FORMAT)
+      }
+    } else {
+      throw new Error('Invalid due date format provided')
+    }
   }
   return convertToDaxDate(dueDate)
 }
