cc32d111c64d4ab59817422918f3c47b8080562e
Oliver Lewington <oliver.lewington@eviden.com>
2025-02-26 16:54:55 +0000

    Commiting changes for v10 pipeline migration

--- app/config/messaging.js
+++ app/config/messaging.js
@@ -7,7 +7,8 @@ const schema = Joi.object({
     username: Joi.string(),
     password: Joi.string(),
     useCredentialChain: Joi.bool().default(false),
-    appInsights: Joi.object()
+    appInsights: Joi.object(),
+    managedIdentityClientId: Joi.string().optional()
   },
   paymentSubscription: {
     address: Joi.string(),
@@ -40,7 +41,8 @@ const config = {
     username: process.env.MESSAGE_QUEUE_USER,
     password: process.env.MESSAGE_QUEUE_PASSWORD,
     useCredentialChain: process.env.NODE_ENV === PRODUCTION,
-    appInsights: process.env.NODE_ENV === PRODUCTION ? require('applicationinsights') : undefined
+    appInsights: process.env.NODE_ENV === PRODUCTION ? require('applicationinsights') : undefined,
+    managedIdentityClientId: process.env.AZURE_CLIENT_ID
   },
   paymentSubscription: {
     address: process.env.PAYMENT_SUBSCRIPTION_ADDRESS,
