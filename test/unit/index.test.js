b528a24b4413ef0a439a0ca663dcadd364c552be
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-12 17:01:57 +0000

    Adding feature flag to allow service to be disabled (#124)
    * Adding feature flag to allow service to be disabled
    
    * update package-lock.json
    
    * 1.29.28
    
    * removing mistake in index.js
    
    ---------
    
    Co-authored-by: ffcplatform <ffcplatforms@environment-agency.gov.uk>

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,12 +1,42 @@
-jest.mock('../../app/messaging')
-const mockMessaging = require('../../app/messaging')
+const { enrichmentConfig } = require('../../app/config')
 
-describe('app', () => {
+jest.mock('../../app/messaging')
+const { start: mockStartMessaging } = require('../../app/messaging')
+
+const startApp = require('../../app')
+
+describe('app start', () => {
   beforeEach(() => {
-    require('../../app')
+    jest.clearAllMocks()
   })
 
-  test('starts messaging', async () => {
-    expect(mockMessaging.start).toHaveBeenCalled()
+  test('starts messaging when active is true', async () => {
+    enrichmentConfig.processingActive = true
+    await startApp()
+    expect(mockStartMessaging).toHaveBeenCalledTimes(1)
+  })
+
+  test('does not start messaging if active is false', async () => {
+    enrichmentConfig.processingActive = false
+    await startApp()
+    expect(mockStartMessaging).toHaveBeenCalledTimes(0)
+  })
+
+  test('does not log console.info when active is true', async () => {
+    enrichmentConfig.processingActive = true
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation(() => {})
+    await startApp()
+    expect(consoleInfoSpy).not.toHaveBeenCalled()
+    consoleInfoSpy.mockRestore()
+  })
+
+  test('logs console.info when active is false', async () => {
+    enrichmentConfig.processingActive = false
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation(() => {})
+    await startApp()
+    expect(consoleInfoSpy).toHaveBeenCalledWith(
+      expect.stringContaining('Processing capabilities are currently not enabled in this environment')
+    )
+    consoleInfoSpy.mockRestore()
   })
 })
