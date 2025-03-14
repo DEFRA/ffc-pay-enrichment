c715718fd1ce1c7f1402564ab01e6f821dd48857
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-14 10:29:56 +0000

    Creating create-server tests

--- test/integration/server/plugins/error.test.js
+++ test/integration/server/plugins/error.test.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi')
+const Boom = require('@hapi/boom')
 const plugin = require('../../../../app/server/plugins/errors').plugin
 
 describe('Errors Plugin', () => {
@@ -10,9 +11,17 @@ describe('Errors Plugin', () => {
 
     server.route({
       method: 'GET',
-      path: '/error',
+      path: '/regular-error',
       handler: () => {
-        throw new Error('Test error')
+        throw new Error('Regular test error')
+      }
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/boom-error',
+      handler: () => {
+        throw Boom.badRequest('Hapi Boom test error')
       }
     })
   })
@@ -21,7 +30,32 @@ describe('Errors Plugin', () => {
     await server.stop()
   })
 
-  test('should log errors when an error response is returned', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('should log errors when a regular error (non-Boom) occurs', async () => {
+    const requestLogSpy = jest.fn()
+    server.ext('onRequest', (request, h) => {
+      request.log = requestLogSpy
+      return h.continue
+    })
+
+    const response = await server.inject({
+      method: 'GET',
+      url: '/regular-error'
+    })
+
+    expect(response.statusCode).toBe(500)
+
+    expect(requestLogSpy).toHaveBeenCalledWith('error', expect.objectContaining({
+      statusCode: 500,
+      message: 'Regular test error',
+      payloadMessage: ''
+    }))
+  })
+
+  test('should log errors when a Boom error occurs (covers isBoom)', async () => {
     const requestLogSpy = jest.fn()
 
     server.ext('onRequest', (request, h) => {
@@ -31,14 +65,14 @@ describe('Errors Plugin', () => {
 
     const response = await server.inject({
       method: 'GET',
-      url: '/error'
+      url: '/boom-error'
     })
 
-    expect(response.statusCode).toBe(500)
+    expect(response.statusCode).toBe(400)
 
     expect(requestLogSpy).toHaveBeenCalledWith('error', expect.objectContaining({
-      statusCode: 500,
-      message: 'Test error',
+      statusCode: 400,
+      message: 'Hapi Boom test error',
       payloadMessage: ''
     }))
   })
