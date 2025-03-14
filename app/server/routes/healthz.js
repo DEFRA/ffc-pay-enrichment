120a8c9037a0692cbcb2c083370afa3139ccb071
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-14 09:59:59 +0000

    Appeasing the Sonar Gods

--- app/server/routes/healthz.js
+++ app/server/routes/healthz.js
@@ -1,9 +1,10 @@
 const { OK } = require('../../constants/ok')
+const { OK: statusOK } = require('../../constants/status-codes')
 
 module.exports = {
   method: 'GET',
   path: '/healthz',
-  handler: (request, h) => {
-    return h.response(OK).code(200)
+  handler: (_request, h) => {
+    return h.response(OK).code(statusOK)
   }
 }
