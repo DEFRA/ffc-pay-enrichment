c55b726b86f10466980051271bc062cc0ceae208
Leigh Godson <leigh.godson@atos.net>
2026-05-21 10:18:04 +0100

    review fixes

--- app/messaging/process-customer-message.js
+++ app/messaging/process-customer-message.js
@@ -1,3 +1,4 @@
+const util = require('util')
 const { saveUpdate } = require('../customer')
 const { sendMessage } = require('./send-message')
 const { CUSTOMER_UPDATE_PROCESSING_FAILED } = require('../constants/types')
@@ -5,7 +6,7 @@ const { CUSTOMER_UPDATE_PROCESSING_FAILED } = require('../constants/types')
 const processCustomerMessage = async (message, receiver) => {
   const update = message.body
   try {
-    console.log(`Customer update received: vendor: ${update.vendor}, sbi: ${update?.sbi}, frn: ${update?.frn}`)
+    console.log('Customer update received:', util.inspect(update, false, null, true))
     if (
       !update?.frn ||
       (!update.vendor && !update.trader && !update.sbi)
@@ -16,7 +17,7 @@ const processCustomerMessage = async (message, receiver) => {
     console.log('Customer update processed')
     await receiver.completeMessage(message)
   } catch (err) {
-    console.error(`Unable to process customer update request: sbi: ${update?.sbi}, frn: ${update?.frn}`)
+    console.error('Unable to process customer update request:', util.inspect(err.message, false, null, true))
     await sendMessage({ update, updated: false, error: err.message }, CUSTOMER_UPDATE_PROCESSING_FAILED, update)
     await receiver.deadLetterMessage(message)
   }
